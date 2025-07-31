ede7f5b7746a4ccaadb5f8c89f2b02119957a395
Jimin Byun <fraise3838@naver.com>
2025-08-01 01:14:50 +0900

    fix : 새로운 사용자 온보딩 오류 해결

--- app/models.py
+++ app/models.py
@@ -9,13 +9,13 @@ class User(Base):
     __tablename__ = "users"
 
     user_id = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False) 
     email = Column(String(255), unique=True, nullable=False)
     is_active = Column(Boolean, default=True, nullable=False)
     name = Column(String(255), nullable=False)
     password = Column(String(255), nullable=False)
     unban_at = Column(DateTime(timezone=True), nullable=True)
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now(), nullable=False)
 
     reactions = relationship("ContentReaction", back_populates="user")
     user_personas = relationship(
@@ -43,8 +43,8 @@ class ContentReaction(Base):
     content_id = Column(BigInteger, index=True)
     reaction = Column(SQLEnum(ReactionType, name='reaction_type_enum', create_type=True), nullable=False)
     type = Column(String(255))
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False) 
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now(), nullable=False)
 
     user = relationship("User", back_populates="reactions") 
 
@@ -52,11 +52,11 @@ class Persona(Base):
     __tablename__ = "personas"
 
     persona_id = Column(BigInteger, primary_key=True, index=True, autoincrement=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False) 
     description = Column(String(255))
     name = Column(String(50))
     tag = Column(String(255))
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now(), nullable=False)
 
     user_personas = relationship("UserPersona", back_populates="persona")
 
@@ -66,8 +66,8 @@ class UserPersona(Base):
     user_id = Column(BigInteger, ForeignKey('users.user_id'), primary_key=True, index=True)
     persona_id = Column(BigInteger, ForeignKey('personas.persona_id'), primary_key=True, index=True)
     score = Column(Float)
-    created_at = Column(DateTime, nullable=False)
-    updated_at = Column(DateTime, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now(), nullable=False)
 
     user = relationship("User", back_populates="user_personas")
     persona = relationship("Persona", back_populates="user_personas")
@@ -84,8 +84,8 @@ class Review(Base):
     like_count = Column(Integer, default=0, nullable=False)
     report_count = Column(Integer, default=0, nullable=False)
     review_status = Column(SQLEnum(ReviewStatus, name='review_status_enum', create_type=True), default=ReviewStatus.COMMON, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False) 
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now(), nullable=False)
 
     user = relationship("User", back_populates="reviews")
 
